d3287d09733ac64253ff1ba9e9e95836f84a1149
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-12-13 16:36:21 -0500

    Add validation to the user model using TDD

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :external_id, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :status, presence: true
+  validates :external_created_at, presence: true
 end
