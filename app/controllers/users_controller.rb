42ff3452f41b318f1b89782d31627cac67dd6825
Ronald Skinner <rjskinnerc@unal.edu.co>
2022-12-14 08:17:47 -0500

    Refactor show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
   end
 
   private
